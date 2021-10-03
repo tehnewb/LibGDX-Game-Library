c28d4bdfae723d99be08256778bec24dd94e9de2
Wildskiller <albert_beaupre@hotmail.com>
2021-10-03 17:33:02 -0400

    LibGDX Project Creation
    The LibGDX program was used to generate these files for the project.

---
+++ desktop/src/game/library/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.library.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.library.LibGDXGameLibrary;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXGameLibrary(), config);
+	}
+}
